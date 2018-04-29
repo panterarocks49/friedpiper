a03aaed656eb68501411cecaf0627469be4ca3f6
Josh Brown <brown.joshua49@aol.com>
2018-04-29 03:24:11 -0700

    connected to swarm

--- src/cljs/friedpiper/ipfs.cljs
+++ src/cljs/friedpiper/ipfs.cljs
@@ -1,6 +1,6 @@
 (ns friedpiper.ipfs
   (:require
-   ["orbit-db" :as orbitDB]
+   ;; ["orbit-db" :as orbitDB]
    [promesa.core :as p]
    [reagent.core :as r])
   (:require-macros
@@ -9,29 +9,39 @@
 
 (def ipfs-options
   (clj->js
-   {:EXPERIMENTAL
-    {:pubsub true}}))
+   {:repo "/fried/piper/data/share"
+    :start true
+    :EXPERIMENTAL
+    {:pubsub true}
+    :config
+    {:Addresses
+     {:Swarm [
+              "/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star"
+              ]}}}))
 
 
 (defn init [app-state]
-
-  ;; (let [*ipfs     (r/cursor app-state [:ipfs])
-  ;;       *orbit-db (r/cursor app-state [:orbit-db])
-  ;;       new-ipfs  (new js/Ipfs ipfs-options)]
-  ;;   (.on new-ipfs "error" (fn [e] (js/console.log e)))
-  ;;   (.on new-ipfs "ready"
-  ;;        (fn []
-  ;;          (p/alet
-  ;;           [orbit-db (new orbitDB new-ipfs)
-  ;;            ;; access   (clj->js
-  ;;            ;; {:write ["*"]})
-  ;;            _ (js/console.log orbit-db)
-  ;;            db       #_(p/await) #_(p/delay 20000000) (.keyvalue orbit-db "first-db")
-  ;;            ]
-  ;;           ;; (reset! *ipfs new-ipfs)
-  ;;           ;; (reset! *orbit-db orbit-db)
-  ;;           (js/console.log db)
-  ;;           )
-  ;;          true)))
-
+  (let [*ipfs     (r/cursor app-state [:ipfs])
+        *orbit-db (r/cursor app-state [:orbit-db])
+        new-ipfs  (new js/Ipfs ipfs-options)]
+    (.on new-ipfs "error" (fn [e] (js/console.log e)))
+    (.on new-ipfs "ready"
+         (fn []
+           (p/alet
+            [orbit-db (new js/OrbitDB new-ipfs)
+             ;; access   (clj->js
+             ;; {:write ["*"]})
+             db       (p/await (.docs orbit-db "first-db"))
+             hash     (p/await (.put db (clj->js {:_id "friedpiperdata" :value [{:db/id 1} {:db/id 2} {:db/id 3}]})))
+             js-info  (.get db "friedpiperdata")
+             info     (js->clj js-info :keywordize-keys true)
+             ]
+            ;; (reset! *ipfs new-ipfs)
+            ;; (reset! *orbit-db orbit-db)
+            (js/console.log orbit-db)
+            (js/console.log db)
+            (js/console.log hash)
+            (println info)
+            )
+           true)))
   )
